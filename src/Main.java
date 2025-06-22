d226d03ec471260a4fe7c1907e21c73dd2122672
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-22 17:02:45 +0900

    calculation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,66 @@
 import java.util.Scanner;
 
+class Calculation {
+    double num1;
+    double num2;
+    double result;
+    char operator;
+
+    public Calculation(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    // 생성자 없이 오버로딩으로? 아니면 숫자만 생성자로 받기? 근데 생성자 게속 반복하면 계속 새로운 객체만 생긴다? 메모리 이슈?
+
+    public double Calculate(char operator) {
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+            break;
+
+            case '-':
+                result = num1 - num2;
+            break;
+
+            case '*':
+                result = num1 * num2;
+            break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                }
+                break;
+
+//            default :
+//                System.out.println("+, -, *, / 중에서 입력해주세요.");
+//                continue;
+
+        }
+
+        return result;
+    }
+}
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static public int inputNumber() {
+        System.out.print("숫자를 입력하세요: ");
+        return scanner.nextInt();
+    }
+
+    static public char inputOperator() {
+        System.out.print("연산자를 입력하세요(+, -, *, /): ");
+        return scanner.next().charAt(0);
+    }
+
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
 
         double num1;
@@ -12,43 +71,16 @@ public class Main {
         boolean stop = true;
 
 
-        System.out.print("숫자를 입력하세요: ");
-        result = scanner.nextDouble();
+        result = inputNumber();
 
         while (stop) {
             num1 = result;
 
-            System.out.print("연산자를 입력하세요(+, -, *, /): ");
-            operator = scanner.next().charAt(0);
+            operator = inputOperator();
+            num2 = inputNumber();
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = scanner.nextDouble();
-
-            switch (operator) {
-                case '+' :
-                    result += num2;
-                    break;
-
-                case '-' :
-                    result -= num2;
-                    break;
-
-                case '*' :
-                    result *= num2;
-                    break;
-
-                case '/' :
-                    if (num2 != 0) {
-                        result /= num2;
-                    } else {
-                        System.out.println("0으로 나눌 수 없습니다.");
-                    }
-                    break;
-
-                default :
-                    System.out.println("+, -, *, / 중에서 입력해주세요.");
-                    continue;
-            }
+            Calculation calculation = new Calculation(num1, num2);
+            result = calculation.Calculate(operator);
 
             System.out.printf("%f %c %f = %f\n", num1, operator, num2, result);
 
@@ -60,7 +92,5 @@ public class Main {
             }
 
         }
-
-
     }
 }
